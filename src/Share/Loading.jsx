2f45fe4d9b3d98eebb006168d04c9f7744d2c967
rohulaminwd <rohulaminwd@gmail.com>
2023-02-01 10:02:46 +0600

    some change

--- src/Share/Loading.jsx
+++ src/Share/Loading.jsx
@@ -5,16 +5,18 @@ import { FidgetSpinner } from 'react-loader-spinner'
 const Loading = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <FidgetSpinner
-        visible={true}
-        height="100"
-        width="100"
-        ariaLabel="dna-loading"
-        wrapperStyle={{}}
-        wrapperClass="dna-wrapper"
-        ballColors={['#ff0000', '#00ff00', '#0000ff']}
-        backgroundColor="#F4442E"
-      />
+      <div className='pb-[200px]'>
+        <FidgetSpinner
+          visible={true}
+          height="150"
+          width="150"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper"
+          ballColors={['#ff0000', '#00ff00', '#0000ff']}
+          backgroundColor="#F4442E"
+        />
+      </div>
     </div>
   );
 };
